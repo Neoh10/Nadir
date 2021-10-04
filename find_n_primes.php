1d86b58df2e769c48945ec399ef313777c6a42c3
neoh10 <n.e.o.h.10@hotmail.com>
2021-10-04 22:33:31 +0200

    04/10 @ 22:33

--- find_n_primes.php
+++ find_n_primes.php
@@ -1,45 +1,50 @@
 <html lang="es">
-<head>
-    <title>Find N prime numbers</title>
-</head>
-<body>
-<form method="post" action="find_n_primes.php">
-    <label>
-        Number:
-        <input type="text" name="num"/>
-    </label>
-    <input type="submit"/>
-</form>
-<div>
-    <?php
-
-    function getDivisors($num){
-        for($i = 1; $i < $num; $i++){
-            if ($num % $i == 0){
-                return $i;
+    <head>
+        <title>Find N prime numbers</title>
+    </head>
+    <body>
+        <form method="post" action="find_n_primes.php">
+            <label>
+                Number:
+                <input type="text" name="num"/>
+            </label>
+            <input type="submit"/>
+        </form>
+        <div>
+            <?php
+            function getDivisors($num){
+                $array = array();
+                for($i = 1; $i <= $num; $i++) {
+                    if ($num % $i == 0) {
+                        $array[] = $i;
+                    }
+                }
+                return $array;
             }
-        }
-    }
 
-    function isPrimeNum($num){
-        $count = 0;
-
-        for ($i = 2; $count < $num / 2; $i++) {
-            if ($num % $i == 0) {
-                return false;
+            function isPrimeNum($num){
+                /*if (count(getDivisors($num)) == 2){
+                    return true;
+                }
+                return false;*/
+                return count(getDivisors($num)) == 2;
             }
-        }
-
-        return true;
-    }
-
-
-    if (isset($_POST["num"])) {
-        $num = intval($_POST["num"]);
-        isPrimeNum($num);
-    }
-    // sssssssssssssssssssssss
-    ?>
-</div>
-</body>
+            if (isset($_POST["num"])) {
+                $num = intval($_POST["num"]);
+                $i = 0;
+                $primes = array();
+                while (count($primes) < $num){
+                    $i++;
+                    if (isPrimeNum($i)){
+                        $primes[] = $i;
+                    }
+                }
+                echo "First ".$num." prime numbers are: <br>";
+                foreach ($primes as $prime) {
+                    echo "- ".$prime."<br>";
+                }
+            }
+            ?>
+        </div>
+    </body>
 </html>
