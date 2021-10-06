a99d35395523da7965d0b89a59709a8d14aca507
Nadir <nel-ouarnaghlihabib@iessonferrer.net>
2021-10-06 19:45:38 +0200

    N perfects

--- christmas_tree.php
+++ christmas_tree.php
@@ -1,10 +1,24 @@
+<html lang="es">
+<head>
+    <title>Christmas tree</title>
+</head>
+<body>
+<form method="post" action="christmas_tree.php">
+    <label>
+        Number of flats:
+        <input type="text" name="num"/>
+    </label>
+    <input type="submit"/>
+</form>
+<div>
 <?php
-for ( $i=1; $i<5; $i++ ) {
+    function getFlats($num){
+        for ($i = 1; $i < $num; $i++) {
+                echo "*";
+            for($j = 0; $j < $i; $j++) {
+                echo "*";
+            }
 
-    for( $j=0; $j<$i; $j++ ) {
-        echo "*";
-
+        }
+        echo "<br/>";
     }
-
-    echo "<br/>";
-}
