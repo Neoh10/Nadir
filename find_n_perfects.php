54aa8d9571e497eab9e84e686f6cdf211d3d8870
Nadir <nel-ouarnaghlihabib@iessonferrer.net>
2021-10-06 18:44:25 +0200

    N perfects

--- find_n_perfects.php
+++ find_n_perfects.php
@@ -13,16 +13,33 @@
 <div>
     <?php
     function getDivisors($num){
-
+        $divisors = array();
+        for($i = 1; $i < $num; $i++) {
+            if ($num % $i == 0) {
+                $divisors[] = $i;
+            }
+        }
+        return $divisors;
     }
 
     function isPerfectNum($num){
-
+        return array_sum(getDivisors($num)) == $num;
     }
 
     if (isset($_POST["num"])) {
         $num = intval($_POST["num"]);
-
+        $i = 0;
+        $perfects = array();
+        while (count($perfects) < $num){
+            $i++;
+            if (isPerfectNum($i)){
+                $perfects[] = $i;
+            }
+        }
+        echo "First ".$num." perfect numbers are: <br>";
+        foreach ($perfects as $perfect) {
+            echo "- ".$perfect."<br>";
+        }
     }
     ?>
 </div>
